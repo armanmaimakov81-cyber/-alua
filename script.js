655400e6d408aaaebdc9251bf332f27d977c9d3a
AGO <armanmaimakov81@gmail.com>
2026-08-14 02:04:24 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 const yesBtn = document.getElementById("yesBtn");
 const noBtn = document.getElementById("noBtn");
+
 const startScreen = document.getElementById("startScreen");
 const birthdayScreen = document.getElementById("birthdayScreen");
+const songScreen = document.getElementById("songScreen");
+
+const songButton = document.getElementById("songButton");
+const backButton = document.getElementById("backButton");
+const backButtonBottom = document.getElementById("backButtonBottom");
 
 const noTexts = [
     "ну пожалуйста",
@@ -19,11 +25,8 @@ noBtn.addEventListener("click", () => {
 
         yesBtn.style.transform = `scale(${1 + noClicks * 0.18})`;
 
-        const newWidth = Math.max(80 - noClicks * 15, 35);
-        const newPadding = Math.max(38 - noClicks * 7, 12);
-
-        noBtn.style.paddingLeft = `${newPadding}px`;
-        noBtn.style.paddingRight = `${newPadding}px`;
+        noBtn.style.paddingLeft = `${Math.max(38 - noClicks * 7, 12)}px`;
+        noBtn.style.paddingRight = `${Math.max(38 - noClicks * 7, 12)}px`;
         noBtn.style.fontSize = `${Math.max(17 - noClicks * 2, 11)}px`;
 
         if (noClicks === 4) {
@@ -31,21 +34,36 @@ noBtn.addEventListener("click", () => {
         }
     }
 });
+
 yesBtn.addEventListener("click", () => {
     startScreen.classList.remove("active");
+    birthdayScreen.classList.add("active");
 
-    setTimeout(() => {
-        birthdayScreen.classList.add("active");
-        window.scrollTo({
-            top: 0,
-            behavior: "smooth"
-        });
-    }, 200);
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
 });
-const songButton = document.getElementById("songButton");
-const playerContainer = document.getElementById("playerContainer");
 
 songButton.addEventListener("click", () => {
-    playerContainer.classList.add("show");
-    songButton.style.display = "none";
+    birthdayScreen.classList.remove("active");
+    songScreen.classList.add("active");
+
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
 });
+
+function returnToBirthday() {
+    songScreen.classList.remove("active");
+    birthdayScreen.classList.add("active");
+
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+}
+
+backButton.addEventListener("click", returnToBirthday);
+backButtonBottom.addEventListener("click", returnToBirthday);
